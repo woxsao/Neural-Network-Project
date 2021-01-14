4faf3051621b3b170e0836361958ec09f1778b9f
Monica Chan <mchan2021@pingry.org>
2021-01-14 09:15:38 -0500

    Forward and Back prop implementation

---
+++ NeuralNet.py
@@ -0,0 +1,121 @@
+import numpy as np
+from numpy.core.fromnumeric import squeeze
+
+
+
+class NeuralNet:
+    def __init__(self, sizes, biases = None, weights = None):
+        self.numLayers = len(sizes)
+        self.sizes = sizes
+        if biases == None and weights == None:
+            self.weights = []
+            self.biases = []
+            for i in range(1, self.numLayers):
+                self.weights.append(np.random.rand(sizes[i],sizes[i-1]))
+                self.biases.append(np.random.rand(sizes[i],1))
+            self.weights = np.array(self.weights)
+            self.biases = np.array(self.biases)
+        else:
+            self.biases = biases
+            self.weights = weights
+        #for sizes [2,3,2] I'd expect for the weights matrix to be []
+        print('weights: ', self.weights)
+        print('biases: ', self.biases)
+    """
+    This function returns the z's and activations at each layer
+    Parameters:
+    - data: the input data points
+    Returns:
+    - list of z's and activations at each layer where layer 0 is the input layer
+    """
+    def forward_propagate(self, data, function_type = 'sigmoid'):
+        inputs = np.array(data)
+        z_list = []
+        activation_list = []
+        activation_list.append(inputs)
+        for layer in range(0, self.numLayers-1):
+            z = activate(inputs, self.weights[layer], self.biases[layer])
+            z_list.append(z) 
+            a1 = activation_func(z, func = function_type)
+            activation_list.append(a1)
+            inputs = a1
+        return z_list, activation_list
+
+
+    def backward_propagation(self, data, expected, l_rate, minibatch_size = 1, function_type = 'sigmoid'):
+        data = np.array(data)
+        z_list, activation_list = self.forward_propagate(data)
+        err = activation_list[-1] - expected
+        error = 0.5 * (1/data.shape[1]) * np.sum((expected-activation_list[-1])**2)
+        deltas = np.ndarray((self.numLayers), dtype = object)
+        db_list = np.ndarray((self.numLayers), dtype = object)
+        dw_list = np.ndarray((self.numLayers), dtype = object)
+        for layer in range(self.numLayers-1, 0,-1):
+            #delta = np.dot(err, sigmoid_prime(z_list[layer-1]))
+            delta = err * activation_prime(z_list[layer-1], func = function_type)
+            delta_sum = np.sum(delta, axis = 1)
+            deltas[layer] = delta
+            db = delta_sum
+            db = db.reshape(db.shape[0],1)
+            db_list[layer] = db
+            dw = np.dot(delta, activation_list[layer-1].T)
+            dw_list[layer] = dw
+            err = np.dot(np.array(self.weights[layer-1]).T, delta)
+            self.biases[layer-1]-=(l_rate/minibatch_size)  * db
+            self.weights[layer-1]-= (l_rate/minibatch_size) * dw
+        return error, deltas[1:], dw_list[1:], db_list[1:]
+
+"""
+This function returns the z's in one layer given the data, the weights, and the biases for the layer.
+Parameters:
+- a: an array of data points of n * m dimensions
+- w: an array of weights that munst be o * n dimensions 
+- b: an array of biases at the layer
+Returns:
+- the list of z's at this particular layer
+"""
+def activate(a, w, b):
+    z = np.add(np.dot(w,a), b)
+    return z
+
+def activation_func(z, func = 'sigmoid'):
+    if func == 'sigmoid':
+        return sigmoid(z)
+    else:
+        return reLU(z)
+def activation_prime(z, func = 'sigmoid'):
+    if func == 'sigmoid':
+        return sigmoid_prime(z)
+    else:
+        return relUprime(z)
+"""
+This function returns  the activations at a layer
+Parameters: 
+- z: The z's at this layer 
+Returns;
+- the list of activations at this layer. 
+"""
+def sigmoid(z):
+    if z.all() >=0:
+        a2 = 1/(1+np.exp(-z)) 
+    else:
+        a2 = np.exp(z)/(1+np.exp(z))
+    return a2
+
+def sigmoid_prime(z):
+    a = sigmoid(z)
+    aprime = a*(1-a)
+    return aprime
+
+def reLU(z):
+    if(z >= 0):
+        return z
+    else:
+        return 0
+
+def relUprime(z):
+    if z >=0:
+        return 1
+    else:
+        return 0
+
---
+++ Runner.py
@@ -0,0 +1,27 @@
+from NeuralNet import NeuralNet
+
+"""x = [[0.05, 0.05, 0.05], [0.1,  0.1,  0.1]]
+b = [[[0.45], [0.45], [0.45]], [[0.8],[0.8]]]
+w = [[[0.15, 0.3], [0.2, 0.35], [0.25, 0.4]], 
+    [[0.5, 0.6, 0.7], [0.55, 0.65, 0.75]]]
+sizes = [2,3,2]
+
+nn = NeuralNet(sizes = sizes)
+z_list, activation_list = nn.forward_propagate(x)
+print("z's: ", z_list)
+print("activations: ", activation_list)"""
+
+x = [[0.05, 0.05, 0.05], [0.1,  0.1,  0.1]]
+y = [[0.01, 0.01,0.01],[0.99,0.99,0.99]]
+w = [[[0.15, 0.3], [0.2, 0.35], [0.25, 0.4]], 
+    [[0.5, 0.6, 0.7], [0.55, 0.65, 0.75]]]
+b = [[[0.45], [0.45], [0.45]], [[0.8],[0.8]]]
+nn = NeuralNet(sizes = [2,3,2], biases = b, weights = w)
+error, delta, dw, db = nn.backward_propagation(x,y,0.5, minibatch_size=3)
+
+print("error: ", error)
+print("deltas: ", delta)
+print("dw's: ", dw)
+print("db: ", db)
+print('weights: ', nn.weights)
+print('biases: ', nn.biases)
